476868de45a0fc90b5fa08b6b79c537fb1d0a8d7
khalil zaarour <k.zaarour1997@gmail.com>
2020-09-30 12:39:16 +0300

    finished-step-7

---
+++ javascript-basics-01/step-7.js
@@ -0,0 +1,11 @@
+var a,b;
+a=prompt("enter shoe size");
+b=prompt("enter birth year");
+
+
+function multiplier(a, b) {
+    return (((a*2)+5)*50)-b;
+}
+var c=multiplier(a, b);
+window.alert("result="+'  '+c);
+
