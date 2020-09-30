e6585c19287f5568704ec91a89f50e913843bba3
khalil zaarour <k.zaarour1997@gmail.com>
2020-09-30 12:12:15 +0300

    finished-step-4

---
+++ javascript-basics-01/step-4.js
@@ -0,0 +1,5 @@
+var name,surname,city;
+name=prompt("enter name plz:");
+surname=prompt("enter surname plz:");
+city=prompt("enter city plz:");
+window.alert("name:"+name+'\n'+"surname:"+surname+'\n'+"city:"+city);
