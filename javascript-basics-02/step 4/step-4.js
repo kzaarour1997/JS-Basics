458ea6b1e14edbfb463982d51f75b878b7554cad
khalil zaarour <k.zaarour1997@gmail.com>
2020-09-30 16:24:24 +0300

    finished-step-4

---
+++ javascript-basics-02/step 4/step-4.js
@@ -0,0 +1,20 @@
+function f(a){
+    a=prompt("type yes if you want to reset")
+    if(Boolean(a)==true){
+        innerHTML=document.querySelector("#name").value="";
+        innerHTML=document.querySelector("#surname").value="";
+        innerHTML=document.querySelector("#city").value="";
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
