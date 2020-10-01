c3cfb5b0e09d991135473039c3d23eea2401cf42
khalil zaarour <k.zaarour1997@gmail.com>
2020-10-01 10:08:29 +0300

    finished-step-2

---
+++ javascript-basics-03/step 2/step-2.js
@@ -0,0 +1,12 @@
+function f(){
+var a= document.getElementById("hide");
+var c= document.getElementById("texte");
+var b= document.getElementById("show");
+
+a.addEventListener("click",function(){  c.style.display="none"  });
+b.addEventListener("click",function(){  c.style.display="block"  });
+
+
+
+
+}
