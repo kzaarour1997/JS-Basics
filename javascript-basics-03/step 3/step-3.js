f528e14849125ce60b22d6980b7abb8027ff0c09
khalil zaarour <k.zaarour1997@gmail.com>
2020-10-01 16:21:49 +0300

    finished-step-3

---
+++ javascript-basics-03/step 3/step-3.js
@@ -0,0 +1,26 @@
+
+
+var a = document.getElementsByClassName("color");
+
+var d = document.getElementById("text")
+
+a[0].addEventListener("click", function () { d.style.color = "green" });
+a[1].addEventListener("click", function () { d.style.color = "red" });
+a[2].addEventListener("click", function () { d.style.color = "blue" });
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
